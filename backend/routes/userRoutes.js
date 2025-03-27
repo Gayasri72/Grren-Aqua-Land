87c40ab8ad88c8265dade479ed0516a104b0890e
Gayasri72 <gayasripethum1@gmail.com>
2025-03-27 09:25:51 +0530

    complete user backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,17 +5,31 @@ import {
   logoutCurrentUser,
   getAllUsers,
   getCurrentUserProfile,
-  updateCurrentUserProfile
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
 } from "../controllers/UserController.js";
 
-import { authenticate,authorizeAdmin } from "../middlewares/authMiddleware.js";
-
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(createUser).get(authenticate,authorizeAdmin,getAllUsers);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
-router.route('/profile').get(authenticate,getCurrentUserProfile).put(authenticate,updateCurrentUserProfile)
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
+// Admin routes
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;
--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -6,13 +6,13 @@ const generateToken = (res, userId) => {
   });
 
   // set JWT as an HTTP-only cookie
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure:process.env.NODE_ENV !== 'development',
-        sameSite:'strict',
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-    });
-    return token
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  });
+  return token;
 };
 
 export default generateToken;
